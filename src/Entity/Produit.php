2314c7fc138454b25ae75f571cbea327cd619480
Khelifa-Yassine <prostam27@gmail.com>
2020-03-21 19:27:17 +0100

    DB create produit table v0

---
+++ src/Entity/Produit.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProduitRepository")
+ */
+class Produit
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $descri;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescri(): ?string
+    {
+        return $this->descri;
+    }
+
+    public function setDescri(?string $descri): self
+    {
+        $this->descri = $descri;
+
+        return $this;
+    }
+}
